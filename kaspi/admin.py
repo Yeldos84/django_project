3bc0bef48d9a6eba8a328d5cdba50544ab5c1995
Сабырбеков Елдос Серикханович <SabyrbekovE@lcoast.itstep>
2024-08-16 22:49:37 +0500

    kaspi

---
+++ kaspi/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Magazine, Good, MagazineGood
+admin.site.register(Magazine)
+admin.site.register(Good)
+admin.site.register(MagazineGood)
+
+# Register your models here.
